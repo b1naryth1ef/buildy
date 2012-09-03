2628706c7f73ef2ad6651b31b369947888dc468c
B1naryth1ef <b1naryth1ef@gmail.com>
2012-09-03 03:09:42 -0500

    Fix worker urls again

--- worker.py
+++ worker.py
@@ -7,7 +7,7 @@ acpt_addr = ['127.0.0.1']
 main_addr = "hydr0.com:5000"#"127.0.0.1:5001"
 our_addr = "127.0.0.1:5001"
 out_addr = "http://hydr0.com/builds/"
-web_dir = "/var/www/builds"
+web_dir = "/var/www/hydr0/builds"
 
 class Job():
     def __init__(self, bid, bcode, info):
